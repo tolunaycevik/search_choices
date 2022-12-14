bf698da30f12bbd49c263646664621a928bb158a
lcuis <louis@jod.li>
2022-12-14 16:22:31 +0100

    restored multiple selection didChange call with a reduced warning so that example 41 continues to pass on the automated testing

--- lib/src/search_choices.dart
+++ lib/src/search_choices.dart
@@ -1151,14 +1151,17 @@ class _SearchChoicesState<T> extends FormFieldState<T> {
   }
 
   void sendSelection(dynamic selection, [BuildContext? onChangeContext]) {
-    if (!widget.multipleSelection) {
-      // We should try to make this work in multiple selection as well
-      // see https://github.com/lcuis/search_choices/issues/97
-      try {
-        didChange(selection);
-      } catch (e, st) {
+    try {
+      didChange(selection);
+    } catch (e, st) {
+      if (!widget.multipleSelection) {
         debugPrint(
             "Warning: didChange call threw an error: ${e.toString()} ${st.toString()} You may want to reconsider the declared types otherwise the form validation may not consider this field properly.");
+      } else {
+        // We should try to make this work in multiple selection as well
+        // see https://github.com/lcuis/search_choices/issues/97
+        debugPrint(
+            "Warning: SearchChoices multipleSelection doesn't fully support Form didChange call.");
       }
     }
     try {
